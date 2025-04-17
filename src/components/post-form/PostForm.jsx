9b705f58c176e2de2d0f775e84914f9b80552071
Dheerap Singh Tanwar <dheerapsinghtanwar360@gmail.com>
2025-04-17 20:28:28 +0530

    latest changes

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -99,62 +99,6 @@ function PostForm({post}) {
     
 
     return (
-//         <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-//         <div className="w-2/3 px-2">
-//             <Input
-//                 label="Title :"
-//                 placeholder="Title"
-//                 className="mb-4"
-//                 {...register("title", { required: true })}
-//             />
-//             <Input
-//                 label="Slug :"
-//                 placeholder="Slug"
-//                 className="mb-4"
-//                 {...register("slug", { required: true })}
-//                 onInput={(e) => {
-//                     setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
-//                 }}
-//             />
-//             <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
-//         </div>
-//         <div className="w-1/3 px-2">
-//             <Input
-//                 label="Featured Image :"
-//                 type="file"
-//                 className="mb-4"
-//                 accept="image/png, image/jpg, image/jpeg, image/gif"
-//                 {...register("image", { required: !post })}
-//             />
-//             {post && (
-//                 <div className="w-full mb-4">
-//                     <img
-//                         src={appwriteService.getFilePreview(post.featuredImage)}
-//                         alt={post.title}
-//                         className="rounded-lg"
-//                     />
-//                 </div>
-//             )}
-//             <Select
-//                 options={["active", "inactive"]}
-//                 label="Status"
-//                 className="mb-4"
-//                 {...register("status", { required: true })}
-//             />
-     
-// <Button
-//     type="submit"
-//     bgColor={post ? "bg-green-500" : "bg-blue-500"}
-//     className={`w-full text-white font-semibold py-2 px-4 rounded-lg transition-all duration-300 ease-in-out ${
-//         post ? "bg-green-500 hover:bg-green-600" : "bg-blue-500 hover:bg-blue-600"
-//     } hover:scale-105 hover:shadow-lg`}
-// >
-//     {post ? "Update" : "Submit"}
-// </Button>
-//         </div>
-//     </form>
-//     )
-
 
 
 
